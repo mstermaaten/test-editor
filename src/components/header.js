d80e2f3d1551f280b87e5a8558f18a3323eaa17c
Marisa Angel <HouseofBo@imac-van-termaaten-familie.local>
2019-08-12 12:02:47 +0200

    header link

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import firebase from "../Firebase";
 import logo from "../static/logo.png";
+import { Link } from "react-router-dom";
+
 
  const Header = () => {
   return (
@@ -14,11 +16,13 @@ import logo from "../static/logo.png";
         >
           <div class="container">
             <div className="menu">
+            <Link to="/">
             <img
               src={logo}
               alt="Logo"
               style={{ width: "250px", padding: "10px 0px" }}
             />
+            </Link>
             <button className="button" onClick={() => firebase.auth().signOut()}>Sign out</button>
             </div>
           </div>
